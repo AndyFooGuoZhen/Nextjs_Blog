a417c6382bdc04bf1ec054653bbc7a04464c4d54
AndyFooGuoZhen <ndfucoding@gmail.com>
2023-04-11 16:42:51 -0500

    Next js setup, md files setup

---
+++ blog/app/page.tsx
@@ -0,0 +1,22 @@
+import fs from "fs";
+
+//Function to read files from /posts
+const getPostMetadata = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs;
+};
+
+const HomePage = () => {
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((slug) => (
+    <div>
+      <h2>{slug}</h2>
+    </div>
+  ));
+  return <div>{postPreviews}</div>;
+};
+
+export default HomePage;
